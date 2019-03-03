129feda66929b92c008ae6e64a33b9987e0ea652
vagrant <vagrant@homestead>
2019-03-03 10:56:22 +0000

    邮箱认证

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,6 +8,13 @@ class PagesController extends Controller
 {
     public function root()
     {
+
         return view('pages.root');
     }
+
+//    public function root()
+//    {
+//        dd(\Auth::user()->hasVerifiedEmail());//验证邮箱是否验证
+//        return view('pages.root');
+//    }
 }
