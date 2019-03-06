afa79bbc42455019021c9620ee233cbd44aa5a43
vagrant <vagrant@homestead>
2019-03-06 08:20:20 +0000

    用户可以发表评论

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -6,6 +6,7 @@ use App\Models\Reply;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReplyRequest;
+use Auth;
 
 class RepliesController extends Controller
 {
@@ -14,40 +15,22 @@ class RepliesController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
-	{
-		$replies = Reply::paginate();
-		return view('replies.index', compact('replies'));
-	}
-
-    public function show(Reply $reply)
-    {
-        return view('replies.show', compact('reply'));
-    }
 
 	public function create(Reply $reply)
 	{
 		return view('replies.create_and_edit', compact('reply'));
 	}
 
-	public function store(ReplyRequest $request)
+	public function store(ReplyRequest $request, Reply $reply)
 	{
-		$reply = Reply::create($request->all());
-		return redirect()->route('replies.show', $reply->id)->with('message', 'Created successfully.');
+        $reply->content = $request->content;
+        $reply->user_id = Auth::id();
+        $reply->topic_id = $request->topic_id;
+        $reply->save();
+		return redirect()->to($reply->topic->link())->with('message', '创建评论成功');
 	}
 
-	public function edit(Reply $reply)
-	{
-        $this->authorize('update', $reply);
-		return view('replies.create_and_edit', compact('reply'));
-	}
 
-	public function update(ReplyRequest $request, Reply $reply)
-	{
-		$this->authorize('update', $reply);
-		$reply->update($request->all());
-		return redirect()->route('replies.show', $reply->id)->with('message', 'Updated successfully.');
-	}
 
 	public function destroy(Reply $reply)
 	{
